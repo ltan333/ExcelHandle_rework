d51bd564160805674ffdd1399cd4d5a40eeb36e4
Anlt <truongan147258@gmail.com>
2023-08-31 17:34:07 +0700

    sơ sơ

--- EmployeeSalaryManagement/Form1.Designer.vb
+++ EmployeeSalaryManagement/Form1.Designer.vb
@@ -22,16 +22,73 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        PanelMainMenu = New Panel()
+        PanelLogo = New Panel()
+        PictureBox1 = New PictureBox()
+        DataGridView1 = New DataGridView()
+        PanelMainMenu.SuspendLayout()
+        PanelLogo.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' PanelMainMenu
+        ' 
+        PanelMainMenu.BackColor = Color.FromArgb(CByte(31), CByte(30), CByte(68))
+        PanelMainMenu.Controls.Add(PanelLogo)
+        PanelMainMenu.Dock = DockStyle.Left
+        PanelMainMenu.Location = New Point(0, 0)
+        PanelMainMenu.Name = "PanelMainMenu"
+        PanelMainMenu.Size = New Size(220, 556)
+        PanelMainMenu.TabIndex = 0
+        ' 
+        ' PanelLogo
+        ' 
+        PanelLogo.Controls.Add(PictureBox1)
+        PanelLogo.Dock = DockStyle.Top
+        PanelLogo.Location = New Point(0, 0)
+        PanelLogo.Name = "PanelLogo"
+        PanelLogo.Size = New Size(220, 140)
+        PanelLogo.TabIndex = 0
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.kanna2
+        PictureBox1.Location = New Point(12, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(0, 0)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(502, 61)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(0, 0)
+        DataGridView1.TabIndex = 1
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1005, 474)
+        ClientSize = New Size(1375, 556)
+        Controls.Add(DataGridView1)
+        Controls.Add(PanelMainMenu)
         Name = "Form1"
         Text = "Form1"
+        PanelMainMenu.ResumeLayout(False)
+        PanelLogo.ResumeLayout(False)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
+    Friend WithEvents PanelMainMenu As Panel
+    Friend WithEvents PanelLogo As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- EmployeeSalaryManagement/Form1.vb
+++ EmployeeSalaryManagement/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles PanelMainMenu.Paint
+
+    End Sub
 End Class
